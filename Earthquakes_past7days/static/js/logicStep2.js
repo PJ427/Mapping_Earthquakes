d583b46ac94b1ee5bf66cca07877a698a4eab68e
Paul Smith <paul.j.smith@email.com>
2021-01-15 17:33:22 -0600

    Made correction to radius

--- Earthquakes_past7days/static/js/logicStep2.js
+++ Earthquakes_past7days/static/js/logicStep2.js
@@ -1,9 +1,6 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
-// Create the map object with a center and zoom level.
-//let map = L.map('mapid').setView([30, 30], 2);
-
 // We create the tile layer that will be the background for our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
   attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -48,7 +45,7 @@ function styleInfo(feature) {
     fillOpacity: 1,
     fillColor: "#ffae42",
     color: "#000000",
-    radius: getRadius(),
+    radius: getRadius(feature.properties.mag),
     stroke: true,
     weight: 0.5
   };
